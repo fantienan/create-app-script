b560b3fa6e734a3d859fa599143fe23fa1f2433f
fantienan <363982607@qq.com>
2020-04-17 16:51:35 +0800

    feat: 优化包结构；添加create、router、page

--- README.md
+++ README.md
@@ -6,11 +6,31 @@
 ## 使用
 ### 使用npx每次运行命令都会拉取最新的create-app
 ```
-npx create-app-script <项目名称>
+npx create-app-script create <app-name>
 ```
 ### 使用全局安装，缺点再次使用时不会更新最新的create-app
 ```
 yarn add create-app-script -g
+create-app-script create <app-name>
+```
+
+### 创建应用
+```
+npx create-app-script create <app-name> 
+npx create-app-script create <app-name> -t
+npx create-app-script create <app-name> -r
+```
+
+### 添加page
+```
+npx create-app-script page <page-name> 
+npx create-app-script page <page-name> -b
+```
+
+### 添加路由
+```
+npx create-app-script router <router-name> 
+npx create-app-script router <router-name> -b
 ```
 
 
--- bin/index.js
+++ bin/index.js
@@ -6,9 +6,6 @@ const semver = require('semver');
 const requiredVersion = require('../package.json').engines.node;
 const currentNodeVersion = process.versions.node;
 
-/**
- * @param {string} wanted - 期望的node版本号
- */
 function checkNodeVersion(wanted, id) {
   if (!semver.satisfies(process.version, wanted)) {
     console.log(chalk.red('You are running Node ' +
@@ -22,4 +19,63 @@ function checkNodeVersion(wanted, id) {
 }
 checkNodeVersion(currentNodeVersion, requiredVersion);
 
-require('./createApp');
+const program = require('commander');
+const minimist = require('minimist');
+
+program
+  .version(require('../package').version)
+  .usage('<command> [options]');
+
+program
+  .command('create <app-name>')
+  .description('create a new project powered by create-app-script-service')
+  .option('-t, --template', 'choice template')
+  .option('-r, --router', 'initial route')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd)
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.log(chalk.yellow(`\nInfo: You provided more than one argument. The first one will be used as the app\'s name, the rest are ignored.`))
+    }
+    require('../lib/create')(name, options)
+  });
+
+program
+  .command('page <page-name>')
+  .description('create a new page powered by create-app-script-service')
+  .option('-b, --batch', 'batch page creation')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd)
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.log(chalk.yellow(`\nInfo: You provided more than one argument. The first one will be used as the page\'s name, the rest are ignored.`))
+    }
+    require('../lib/createPage')(name, options)
+  })
+
+program
+  .command('router <router-name>')
+  .description('create a new router powered by create-app-script-service')
+  .option('-b, --batch', 'batch router creation')
+  .action((name, cmd) => {
+    const options = cleanArgs(cmd)
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.log(chalk.yellow(`\nInfo: You provided more than one argument. The first one will be used as the page\'s name, the rest are ignored.`))
+    }
+    require('../lib/createRouter')(name, options)
+  })
+
+program.parse(process.argv)
+
+function camelize(str) {
+  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+}
+
+function cleanArgs(cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
